fa82b00881630bc55e8ee2537bfe876ff88a87ac
Luke Archer <l.archer@leeds.ac.uk>
2023-01-24 10:21:19 +0000

    Updated all data generation scripts

--- minos/data_generation/US_complete_case.py
+++ minos/data_generation/US_complete_case.py
@@ -61,20 +61,24 @@ if __name__ == "__main__":
     file_names = [f"data/composite_US/{item}_US_cohort.csv" for item in years]
     data = US_utils.load_multiple_data(file_names)
 
-    complete_case_vars = ["housing_quality", 'job_sector', 'marital_status', 'labour_state', 'yearly_energy', "job_sec",
-                          "education_state", 'region', "age"] # many of these
+    complete_case_vars = ["housing_quality", 'marital_status', 'yearly_energy', 'job_sector', "job_sec",
+                          "education_state", 'region', "age", "SF_12"]  # many of these
+    # removed: labour_state
 
 
     data = complete_case_varlist(data, complete_case_vars)
 
     ## Need to do correction on some variables individually as they are only in the dataset in specific years
     # doing complete case without the year range taken into account removes the whole years data
-    data = complete_case_custom_years(data, 'loneliness', years = [2018, 2019, 2020])
+    data = complete_case_custom_years(data, 'loneliness', years=[2017, 2018, 2019, 2020])
     # now make sure its int not float (need to convert NA to 0 for this to work)
-    data['loneliness'] = pd.to_numeric(data['loneliness'], errors='coerce').fillna(0).astype('int')
+    data['loneliness'] = pd.to_numeric(data['loneliness'], errors='coerce').fillna(2).astype('int')
     ## Now do same for neighbourhood_safety
     data = complete_case_custom_years(data, 'neighbourhood_safety', years=[2011, 2014, 2017, 2018])
-    data['neighbourhood_safety'] = pd.to_numeric(data['neighbourhood_safety'], errors='coerce').fillna(0).astype('int')
+    data['neighbourhood_safety'] = pd.to_numeric(data['neighbourhood_safety'], errors='coerce').fillna(2).astype('int')
+    # ncigs missing for wave 1 only
+    data = complete_case_custom_years(data, 'ncigs', years=list(range(2010, 2020, 1)))
+    data['ncigs'] = pd.to_numeric(data['ncigs'], errors='coerce').fillna(0).astype('int')
 
     drop_columns = ['financial_situation', # these are just SF12 MICE columns for now. see US_format_raw.py
                     'ghq_depression',
---
+++ minos/data_generation/US_scotland_subsetting.py
@@ -0,0 +1,69 @@
+""" File for subsetting US data. If only interested in fraction of population as input. """
+
+import US_utils
+import pandas as pd
+import numpy as np
+
+def get_scottish(data):
+    """
+
+    Parameters
+    ----------
+    data
+
+    Returns
+    -------
+
+    """
+    return data.loc[data['region'] == 'Scotland', ]
+
+#def main():
+#    # get files
+#    # subset by X.
+#    # save files.
+#    file_names = ["data/upscaled_US/2018_US_cohort.csv"]
+#    data = US_utils.load_multiple_data(file_names)[0]
+#    data =get_scottish(data)
+#    US_utils.save_multiple_files(data, [2018], "subsetted_US", "")
+
+
+def handle_small_samples(data):
+    """
+    Scot data has much smaller sample than the whole UK, and therefore some transition models struggled. We need to
+    combine some groups in order to get Scotland mode working.
+    Parameters
+    ----------
+    data
+
+    Returns
+    -------
+
+    """
+
+    ## FIRST deal with ethnic group
+    # Need to replace all the groups with a white/non-white split as minority ethnic groups are too small number
+    #data['ethnicity'][~data['ethnicity'].isin(['WBI', 'WHO'])] = 'Non-White'
+    #data['ethnicity'][data['ethnicity'].isin(['WBI', 'WHO'])] = 'White'
+
+    # JOB_SEC breaking the loneliness model
+    # JOO_SEC also breaking the ncigs model
+
+    # First deal with education_state == 6 - very small numbers in this group. Combine with level
+
+    return data
+
+
+def main():
+    # get files
+    # subset by X.
+    # save files.
+
+    years = np.arange(2009, 2020) # only saving UKHLS data after 2009.
+    file_names = [f"data/final_US/{item}_US_cohort.csv" for item in years]
+    data = US_utils.load_multiple_data(file_names)
+    data = get_scottish(data)
+    data = handle_small_samples(data)
+    US_utils.save_multiple_files(data, years, "data/scotland_US/", "")
+
+if __name__ == '__main__':
+    main()
